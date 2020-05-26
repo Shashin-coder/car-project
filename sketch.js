79958860fb5917455c49461cbf78199b494c42eb
Shashin-coder <65901465+Shashin-coder@users.noreply.github.com>
2020-05-26 21:03:54 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,42 @@
-var car,wall;
+var car,wall,Speed,weight;
 
-var speed=random(55,90);
-var weight=random(500,1500);
+
 
 
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(1400,400);
   car=createSprite(50,200,30,20)
-  car.shapeColor="blue";
-wall=createSprite(370,200,30,400);
-wall.shapeColor="blue";
+  
+wall=createSprite(1300,200,60,height/2);
+
+Speed=random(70,200);
+weight=random(500,3000)
 
 }
 
 function draw() {
   background(0); 
- 
- car.velocityX=speed;
-  if(wall.x-car.x<car.width/2+wall.width/2){
-    
-    var deformation=0.5*speed*weight/22500;
-    if(deformation>180){
-      car.shapeColor="red"
-    }
-    if(deformation<180){
-      car.shapeColor="yellow"
-    }
-    if(deformation>100){
-      car.shapeColor="green";
-    }
+  
+ car.velocityX=Speed;
+  if(wall.x-car.x < car.width/2+wall.width/2){
+   car.velocityX=0;
+   var destruction=0.5*Speed*weight/22500;
+   
+   if(destruction<50) {
+     car.shapeColor=color(0,255,0)
+    wall.shapeColor=color(0,255,0)
+   }
+   if(destruction<75) {
+    car.shapeColor=color(223,230,0);
+    wall.shapeColor=color(223,230,0);
+  }
+  if(destruction>100) {
+    car.shapeColor=color(255,0,0);
+    wall.shapeColor=color(255,0,0);
+  }
+   
+  
   }
   drawSprites();
 }
